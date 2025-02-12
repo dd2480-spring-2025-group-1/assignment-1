489b1b1d657d25d852c5ffa9f1a52bd595f5e033
Celleforst <77944684+Celleforst@users.noreply.github.com>
2025-02-12 20:04:57 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ parameters: Parameters  # Used to derive CMV
 lcm: list[list[Connectors]]  # Used with CMV to derive PUM
 puv: list[bool]  # Used with PUM to derive LAUNCH
 verbose: bool = False
-hi = 0
 
 # Helper functions
 def get_cli_args() -> tuple[str, bool]:
